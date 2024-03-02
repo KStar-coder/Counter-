0e96d1dce1c9baa73c6c139035c14ae4f8bbc8ee
KStar-coder <sharmakaustav04@gmail.com>
2024-03-02 09:35:13 +0530

    Removed unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   // hook to change the state : useState. it returns 2 values, a variable and a function
-  //   we can name these 2 anything we want 
-  let [counter, setCounter] = useState(0) // 0 will be the default value
+  //   we can name these 2 anything we want
+  let [counter, setCounter] = useState(0); // 0 will be the default value
 
   //let counter = 0
   const addValue = () => {
-    console.log("working")
+    console.log("working");
     // counter = counter + 1
-    setCounter(counter + 1)
-  }
+    setCounter(counter + 1);
+  };
 
   const subtractValue = () => {
-    if (counter >= 1)
-      setCounter(counter - 1)
-  }
+    if (counter >= 1) setCounter(counter - 1);
+  };
 
   return (
     <>
       <h1>Chai aur React</h1>
       <h2>Counter Value : {counter}</h2>
-      <button
-        onClick={addValue}
-      >Add Value</button>
+      <button onClick={addValue}>Add Value</button>
       <br />
-      <button
-        onClick={subtractValue}
-      >Decrease Value</button>
+      <button onClick={subtractValue}>Decrease Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
